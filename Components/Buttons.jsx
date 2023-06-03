875a44839b1b9e11a3435914fb0c8472afb08e90
000 <57313924+BooksHorse@users.noreply.github.com>
2023-06-04 00:56:19 +0700

    sss

--- Components/Buttons.jsx
+++ Components/Buttons.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { View, StyleSheet } from "react-native";
 import {Button,Text} from  'react-native-paper';
 import {pb} from '../Plugins/pocketbase'
-import { useTheme } from 'react-native-paper';
+import { useTheme,FAB } from 'react-native-paper';
 
 export function ButtonsChoice({choices,press}) {
   
@@ -12,14 +12,14 @@ export function ButtonsChoice({choices,press}) {
     <View >
       <View style={style.row}>
           <View style={style.gridContainer}>
-            <Button contentStyle={{margin:5,height:80}} style={{margin:5}}  mode="contained" onPress={() =>press(0,choices.correct)} title={choices.choice[0]|| "null0" }>{choices.choice[0]|| "null0" }</Button>
-            <Button contentStyle={{margin:5,height:80}} style={{margin:5}} mode="contained" onPress={() => press(1,choices.correct)} title={choices.choice[1] || "null1"}>{choices.choice[1] || "null1"}</Button>
+            <FAB contentStyle={{margin:5,height:100}} size="small" numberOfLines={5} labelStyle={{ flexShrink: 1 }} style={{margin:5}}  mode="contained" onPress={() =>press(0,choices.correct)} label={choices.choice[0]|| "null0" }>{choices.choice[0]|| "null0" }</FAB>
+            <FAB contentStyle={{margin:5,height:100}} size="small" labelStyle={{ flexShrink: 1 }}  style={{margin:5}} mode="contained" onPress={() => press(1,choices.correct)} label={choices.choice[1] || "null1"}>{choices.choice[1] || "null1"}</FAB>
           </View>
       </View>
       <View style={style.row}>
           <View style={style.gridContainer}>
-            <Button contentStyle={{margin:5,height:80}}style={{margin:5}} mode="contained" onPress={() => press(2,choices.correct)} title={choices.choice[2]  || "null2"}>{choices.choice[2]  || "null2"}</Button>
-            <Button contentStyle={{margin:5,height:80}} style={{margin:5}} mode="contained" onPress={() =>press(3,choices.correct)} title={choices.choice[3] || "null3"}>{choices.choice[3] || "null3"}</Button>
+            <FAB contentStyle={{margin:5,height:100}} size="small" labelStyle={{ flexShrink: 1 }}  numberOfLines={1} style={{margin:5}} mode="contained" onPress={() => press(2,choices.correct)} label={choices.choice[2]  || "null2"}>{choices.choice[2]  || "null2"}</FAB>
+            <FAB contentStyle={{margin:5,height:100}} size="small" labelStyle={{ flexShrink: 1 }}  style={{margin:5}} mode="contained" onPress={() =>press(3,choices.correct)} label={choices.choice[3] || "null3"}>{choices.choice[3] || "null3"}</FAB>
           </View>
       </View>
     </View>
