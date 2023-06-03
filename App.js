ec64fac2f9668045c19e6e26bfd99e6c9cfcca88
000 <57313924+BooksHorse@users.noreply.github.com>
2023-06-03 10:26:57 +0700

    asd

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import GameScreen from './pages/GameScreen';
+import StartScreen from './pages/StartScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='GameScreen' component={GameScreen} />
+        <Stack.Screen name='StartScreen' component={StartScreen}  /> 
+        {/* options={{ title: "Home | MobileAnt App", headerBackVisible: false }} */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
