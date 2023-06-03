6dd56ab437e150710e90063242e2d1f4d6fb9d21
BooksHorse <57313924+BooksHorse@users.noreply.github.com>
2023-06-03 14:40:38 +0700

    Gamescreen (#3)
    * dadad
    
    * half done

---
+++ Pages/GameScreen.jsx
@@ -0,0 +1,92 @@
+import * as React from "react";
+import { Button, View, StyleSheet, Text } from "react-native";
+import { ButtonsChoice } from "../Components/Buttons";
+import { pb } from "../Plugins/pocketbase";
+import {DeviceEventEmitter} from "react-native"
+import { scoreContext } from "../App";
+
+export function GameScreen({route,navigation}) {
+  let ff =React.useContext(scoreContext);
+  /**
+   * @type {name:string,content:{choices:string[4],correct:number}}
+   */
+  const [questions, setquestions] = React.useState([]);
+  const [title, setTitle] = React.useState("");
+  const [currentQuestionNo, setcurrentQuestionNo] = React.useState(0);
+  const [choice, setChoice] = React.useState("");
+  const [a, seta] = React.useState(false);
+  React.useEffect(() => {
+    pb.collection("question")
+      .getFullList()
+      .then((record) => {
+        setquestions(record);
+        console.log("record:", record);
+        return record;
+      })
+      .then((record) => {
+        setTitle(record[currentQuestionNo].title);
+        setChoice(record[currentQuestionNo].content);
+        console.log("sdad", record[currentQuestionNo]);
+      }).catch(e => console.error("err",e))
+      .finally(() => {
+        seta(true);
+      });
+  }, []);
+
+  const [selectedChoice, setselectedChoice] = React.useState(-1);
+
+  function onPress(choice, correct) {
+    // setTitle(questions[currentQuestionNo].title);
+    // setselectedChoice(choice);
+    console.log(choice);
+    if (choice === correct) {
+      console.log("correct");
+      DeviceEventEmitter.emit("event.scored",{});
+      if (currentQuestionNo + 1 < questions.length) {
+        setChoice(questions[currentQuestionNo+1].content);
+        setTitle(questions[currentQuestionNo+1].title);
+        setcurrentQuestionNo(currentQuestionNo + 1);
+        
+      } else {
+        console.log("game done");
+        //finish game
+      }
+      
+    }
+  }
+
+  function changequestion() {
+    setTitle(questions[currentQuestionNo].title);
+  }
+  console.log("aaaaa",a === true && questions)
+  if (a === true && questions) {
+    // console.log("dadadaddad",questions[0].content)
+    return (
+      <View>
+        <Text>current choice :{selectedChoice}</Text>
+
+        <Text>q no :{currentQuestionNo}</Text>
+        <Text>score:{ff}</Text>
+
+        <Text>{title}</Text>
+
+        <ButtonsChoice choices={choice} press={onPress}></ButtonsChoice>
+      </View>
+    );
+  } else {
+    <View>
+      <text>Loading</text>
+    </View>;
+  }
+}
+
+const style = StyleSheet.create({
+  gridContainer: {
+    flex: 2,
+    marginHorizontal: "auto",
+    width: 400,
+  },
+  row: {
+    flexDirection: "row",
+  },
+});
