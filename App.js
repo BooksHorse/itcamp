ffc046c66cbad5d2e5a7eba728b45c14391abab3
abc <57313924+BooksHorse@users.noreply.github.com>
2023-06-03 14:39:19 +0700

    startscreen2

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import GameScreen from './pages/GameScreen';
-import StartScreen from './pages/StartScreen';
+
+import {StartScreen} from './Pages/StartScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='GameScreen' component={GameScreen} />
+      <Stack.Navigator initialRouteName='StartScreen'>
         <Stack.Screen name='StartScreen' component={StartScreen}  /> 
         {/* options={{ title: "Home | MobileAnt App", headerBackVisible: false }} */}
       </Stack.Navigator>
--- Pages/StartScreen.jsx
+++ Pages/StartScreen.jsx
@@ -1,11 +1,106 @@
 import * as React from 'react';
+import { Button , View,Text,StyleSheet, TouchableOpacity} from 'react-native';
+import { Card } from '@rneui/themed';
 
 
-
-export default function App() {
+export function StartScreen({navigation}) {
     return (
+        <View >
+            <Card style = {style.Main}>
+                <Text style = {style.CardTitle}>Genius</Text>
+
+                {buttonyes("START",() => navigation.navigate('GameScreen'))} 
+                {buttonyes("ADMIN",() => navigation.navigate('GameScreen'))} 
+                
+            </Card>
+        </View>
 
     );
-  }
-  
-  
+}
+
+
+
+const style = StyleSheet.create({
+    Main:{
+        width: 200,
+        height: 100,
+        alignItems: "center",
+        justifyContent: "center",
+        flex: 1,
+        borderRadius: 20,
+
+    },
+
+    CardTitle:{
+        textAlign: "center",
+        fontWeight: "bold",
+        fontSize: 15,
+    },
+
+    Button: {
+        backgroundColor: "#1111",
+        borderRadius: 5,
+        alignItems: "center",
+        margin: 10,
+        padding: 10
+        
+    }
+
+
+})
+
+
+
+
+function buttonyes(text,func) {
+    return <TouchableOpacity style={style.Button}
+        onPress={func}>
+        <Text>{text}</Text>
+    </TouchableOpacity>;
+}
+// export function StartScreen({navigation}) {
+//     return (
+//         <View style = {style.container}>
+
+//             {/* <View style={style.container}> */}
+//                 <Card>
+//                     <Card.Title>Genius</Card.Title>
+                    
+//                     <TouchableOpacity style = {style.Button} 
+//                         onPress={() => navigation.navigate('GameScreen')}>
+//                         <Text>START</Text>
+//                     </TouchableOpacity> 
+//                 </Card>
+//             {/* </View> */}
+            
+//         </View>
+
+//     );
+// }
+
+
+
+
+// const style =StyleSheet.create({
+//     Title:{
+//         textAlign: "center",
+//         fontSize: 70,
+//         fontWeight: "bold",
+//         marginBottom: 20,
+//     },
+
+//     container:{
+//         alignContent: "center",
+//         flex: 1,
+        
+//     },
+    
+//     Button:{
+//         alignItems: "center",
+//         backgroundColor: '#1111',
+//         padding: 10,
+//         borderRadius: 30,
+//     },
+   
+// });
+    
